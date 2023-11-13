7c7fe3c40cf82646b6e03ac88b7b4c01370f7f65
Bara1466 <alprobarani1466@gmail.com>
2023-11-13 08:15:10 +0700

    percabangan

---
+++ Tugas/indek.php
@@ -0,0 +1,21 @@
+<?php
+
+$umur = 35;
+echo "Diketahui umur : 35 tahun ";
+echo "\n";
+if ($umur > 50){
+echo "Termasuk kategori TUA ";
+echo "\n";
+} else if ($umur > 25) {
+echo "Termasuk kategori DEWASA ";
+echo "\n";
+} else if ($umur > 15) {
+echo "Termasuk kategori REMAJA ";
+echo "\n";
+} else {
+echo "Termasuk kategori ANAK ANAK ";
+echo "\n";
+}
+echo "jika($umur > 25) dieksekusi";
+
+?>
